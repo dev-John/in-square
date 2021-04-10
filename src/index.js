5fe4c27cb0945d31b3ed378f638c25023ce3573d
Jonatas <jonatasoliveira1234@gmail.com>
2021-04-10 15:02:28 -0300

    + Swagger and JsDocs

--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@ import cors from "cors";
 import { routes } from "./routes.js";
 import { handleError } from "./utils/error-handler.js";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+import { swaggerOptions } from "./config/swagger.js";
+
 const app = express();
 
+const swaggerDocument = await swaggerJsDoc(swaggerOptions);
+const port = process.env.PORT || 3333;
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, { explorer: true })
+);
 app.use(cors());
 app.use(express.json());
 app.use(routes);
@@ -13,9 +25,9 @@ app.use((err, res) => {
   handleError(err, res);
 });
 
-app.listen(process.env.PORT || 3333);
-
-console.log("Server running on 3333");
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
 
 process.on("unhandledRejection", (err) => {
   console.log(err);
